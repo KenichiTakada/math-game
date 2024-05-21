b521a6176a5b573dc54182d07ad58d66dfd3fd0b
KenichiTakada <59554197+KenichiTakada@users.noreply.github.com>
2024-05-21 21:39:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -176,12 +176,18 @@ function downloadResults() {
     document.body.removeChild(link);
 }
 
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('hidden');
-    menu.classList.toggle('show');
-}
-
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const acc = document.getElementsByClassName("accordion");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
     generateQuestion();
-};
+});
